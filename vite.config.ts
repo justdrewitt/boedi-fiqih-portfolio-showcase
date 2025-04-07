c0d45f344e76ec2e5dc7e360b5f3bfd8a031de76
justdrewit <justtdrewit@gmail.com>
2025-04-07 14:00:44 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/",
   server: {
     host: "::",
     port: 8080,
@@ -41,6 +42,7 @@ export default defineConfig(({ mode }) => ({
         },
       },
     },
+    assetsInclude: ['**/*.webp', '**/*.woff2'],
   },
   optimizeDeps: {
     include: [
