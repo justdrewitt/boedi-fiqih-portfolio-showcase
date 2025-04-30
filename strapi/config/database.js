e7e490aa77b40bf8eb0a6bcfe4b7f1000c967d48
justdrewit <justtdrewit@gmail.com>
2025-05-01 00:13:55 +0700

    update

--- strapi/config/database.js
+++ strapi/config/database.js
@@ -2,8 +2,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      connectionString: env('DATABASE_URL'),
-      ssl: { rejectUnauthorized: false }
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      ssl: env.bool('DATABASE_SSL', false),
     },
   },
 }); 
