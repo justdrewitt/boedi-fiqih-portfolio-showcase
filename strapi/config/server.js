7968a78a8cf5133d497dc80fb6325ed2aa769724
justdrewit <justtdrewit@gmail.com>
2025-05-01 00:26:30 +0700

    update2

--- strapi/config/server.js
+++ strapi/config/server.js
@@ -4,4 +4,9 @@ module.exports = ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS', ['testKey1', 'testKey2']),
   },
+  url: env('PUBLIC_URL', 'https://boedi-fiqih.up.railway.app'),
+  proxy: true,
+  cron: {
+    enabled: false
+  },
 }); 
